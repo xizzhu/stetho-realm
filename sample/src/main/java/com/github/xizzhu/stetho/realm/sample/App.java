efface63bc59b54921302c48389ffb6155834617
Steven Zhu <xizhi.zhu@gmail.com>
2017-05-25 21:54:02 +0300

    Initial commit.

---
+++ sample/src/main/java/com/github/xizzhu/stetho/realm/sample/App.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2017 Xizhi Zhu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.xizzhu.stetho.realm.sample;
+
+import android.app.Application;
+import com.facebook.stetho.Stetho;
+import io.realm.Realm;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+        Stetho.initialize(Stetho.newInitializerBuilder(this)
+            .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+            .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+            .build());
+    }
+}
